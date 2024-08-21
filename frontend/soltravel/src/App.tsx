0cf6d25c1aef165d23e6d810d02f5734505995af
elumiryy <yerim5988jg@naver.com>
2024-08-21 15:29:02 +0900

    [FE] Design: Header 디자인
    - 로고 임시 제작
    - Header 상단 고정

--- frontend/soltravel/src/App.tsx
+++ frontend/soltravel/src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../src/pages/MainPage";
 import Login from "./pages/user/Login";
 import SignUp from "./pages/user/SignUp";
+import Header from "./components/common/Header";
 
 function App() {
   return (
     <div className="h-full">
       <BrowserRouter>
-        {/* <Header></Header> */}
+        <Header></Header>
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/login" element={<Login />}></Route>
