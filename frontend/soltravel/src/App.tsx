a0fe3a82a03e63f4d8a1ba16c12cc9568f05cbd8
박예진 <eksk9044@naver.com>
2024-08-21 11:19:42 +0900

    [FE] Design: 로그인 페이지 디자 - 로그인 페이지 아이디, 사용자 암호 input 및 로그인 버튼 디자인 - 가입하기 버튼을 통한 회원가입 유도 버튼 디자인

--- frontend/soltravel/src/App.tsx
+++ frontend/soltravel/src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../src/pages/MainPage";
+import Login from "./pages/user/Login";
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-full">
       <BrowserRouter>
         {/* <Header></Header> */}
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
