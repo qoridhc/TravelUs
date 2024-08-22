78adf9cf602ddfcd7203073333ff892cdb902526
이성현 <vkse1934@naver.com>
2024-08-22 16:41:33 +0900

    [BE] Feat: 계좌 입금 구현

---
+++ backend/src/main/java/com/ssafy/soltravel/controller/TransactionController.java
@@ -0,0 +1,33 @@
+package com.ssafy.soltravel.controller;
+
+import com.ssafy.soltravel.dto.transaction.request.TransactionRequestDto;
+import com.ssafy.soltravel.dto.transaction.response.DepositResponseDto;
+import com.ssafy.soltravel.service.transaction.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    // 계좌 입금
+    @PostMapping("/{accountNo}/deposit")
+    public ResponseEntity<DepositResponseDto> postAccountDeposit(
+        @PathVariable String accountNo,
+        @RequestBody TransactionRequestDto requestDto
+    ) {
+        ResponseEntity<DepositResponseDto> response = transactionService.postAccountDeposit(accountNo, requestDto);
+
+        return response;
+    }
+
+
+}
---
+++ backend/src/main/java/com/ssafy/soltravel/service/transaction/TransactionService.java
@@ -0,0 +1,63 @@
+package com.ssafy.soltravel.service.transaction;
+
+import com.ssafy.soltravel.common.Header;
+import com.ssafy.soltravel.dto.transaction.request.TransactionRequestDto;
+import com.ssafy.soltravel.dto.transaction.response.DepositResponseDto;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TransactionService {
+
+    private final Map<String, String> apiKeys;
+    private final WebClient webClient;
+    private final ModelMapper modelMapper;
+
+    private final String BASE_URL = "https://finopenapi.ssafy.io/ssafy/api/v1/edu/demandDeposit";
+
+    public ResponseEntity<DepositResponseDto> postAccountDeposit(String accountNo, TransactionRequestDto requestDto) {
+
+        String API_NAME = "updateDemandDepositAccountDeposit";
+        String API_URL = BASE_URL + "/" + API_NAME;
+
+        Header header = Header.builder()
+            .apiName(API_NAME)
+            .apiServiceCode(API_NAME)
+            .apiKey(apiKeys.get("API_KEY"))
+            .userKey(apiKeys.get("USER_KEY"))
+            .build();
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("Header", header);
+        body.put("accountNo", accountNo);
+        body.put("transactionBalance", requestDto.getTransactionBalance());
+        body.put("transactionSummary", requestDto.getTransactionSummary());
+
+        ResponseEntity<Map<String, Object>> response = webClient.post()
+            .uri(API_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(body)
+            .retrieve()
+            .toEntity(new ParameterizedTypeReference<Map<String, Object>>() {
+            })
+            .block();
+
+        Object recObject = response.getBody().get("REC");
+
+        DepositResponseDto depositResponseDto = modelMapper.map(recObject, DepositResponseDto.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(depositResponseDto);
+    }
+}
