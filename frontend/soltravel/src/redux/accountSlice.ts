c58c9c23df86498ea62accc9bfab67f6dbca76c4
elumiryy <yerim5988jg@naver.com>
2024-08-28 16:42:12 +0900

    [FE] Refactor: 계좌 redux 슬라이스의 모임통장목록 정보 타입 리팩토링

--- frontend/soltravel/src/redux/accountSlice.ts
+++ frontend/soltravel/src/redux/accountSlice.ts
@@ -1,54 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { MeetingAccount } from "../types/account";
 
 export interface AccountState {
   isKeyboard?: boolean;
   accountPassword?: string;
-  meetingAccountList: Array<{
-    MeetingAccountName: string;
-    MeetingAccountIcon: string;
-    normalMeetingAccount: {
-      accountNumber: string;
-      accountMoney: string;
-    };
-    foreignMeetingAccount: {
-      accountNumber: string;
-      accountMoney: string;
-      currencyType: string;
-    };
-  }>;
+  meetingAccountList: Array<MeetingAccount>;
 }
 
 const initialState: AccountState = {
   isKeyboard: false,
   accountPassword: "",
   meetingAccountList: [
-    {
-      MeetingAccountName: "모히또에서 몰디브 한 잔하는 모임",
-      MeetingAccountIcon: "airplane",
-      normalMeetingAccount: {
-        accountNumber: "217-879928-13289",
-        accountMoney: "3,481,900",
-      },
-      foreignMeetingAccount: {
-        accountNumber: "212-123428-13289",
-        accountMoney: "113,890",
-        currencyType: "￥",
-      },
-    },
-    {
-      MeetingAccountName: "신암고 1-3반 동창회",
-      MeetingAccountIcon: "school",
-      normalMeetingAccount: {
-        accountNumber: "217-874218-12289",
-        accountMoney: "481,900",
-      },
-      foreignMeetingAccount: {
-        accountNumber: "212-123902-09281",
-        accountMoney: "390",
-        currencyType: "$",
-      },
-    },
   ],
 };
 
@@ -62,12 +25,12 @@ export const userSilce = createSlice({
     setAccountPassword: (state, action: PayloadAction<string>) => {
       state.accountPassword = action.payload;
     },
-    setMeetingAccountList: (state, action: PayloadAction<typeof initialState.meetingAccountList>) => {
+    editMeetingAccountList: (state, action: PayloadAction<Array<MeetingAccount>>) => {
       state.meetingAccountList = action.payload;
     },
   },
 });
 
-export const { setIsKeyboard, setMeetingAccountList, setAccountPassword } = userSilce.actions;
+export const { setIsKeyboard, editMeetingAccountList, setAccountPassword } = userSilce.actions;
 
 export default userSilce.reducer;
