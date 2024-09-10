74b80d52895a55cfff688df5697a7f5b8505fabe
이성현 <vkse1934@naver.com>
2024-09-10 16:28:23 +0900

    [BE] Feat: 일반 개인 계좌 생성 로직 추가

---
+++ bank/src/main/java/com/goofy/tunabank/controller/AccountController.java
@@ -0,0 +1,33 @@
+package com.goofy.tunabank.controller;
+
+import com.goofy.tunabank.dto.ResponseDto;
+import com.goofy.tunabank.dto.account.request.CreateAccountRequestDto;
+import com.goofy.tunabank.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController("/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<ResponseDto> crateNewAccount(
+        @RequestBody CreateAccountRequestDto requestDto
+    ) {
+
+        ResponseDto responseDto = accountService.crateNewAccount(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+
+
+}
