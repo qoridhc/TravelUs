6249ed027736217b50f24569dc27119a64d6f801
이진주 <dlwlswn3539@naver.com>
2024-09-13 13:40:24 +0900

    [BANK] Build: api 버전 분리

--- bank/src/main/java/com/goofy/tunabank/domain/Enum/MerchantCategory.java
+++
@@ -1,7 +0,0 @@
-package com.goofy.tunabank.domain.Enum;
-
-public enum MerchantCategory {
-
-  //FOOD:음식점, ACCOMODATION:숙박, TRANSPORTATION:교통, SHOPPING:쇼핑, OTHER: 기타
-  FOOD,ACCOMODATION,TRANSPORTATION,SHOPPING,OTHER
-}
---
+++ bank/src/main/java/com/goofy/tunabank/v1/account/InvalidAccountIdOrTypeException.java
@@ -0,0 +1,10 @@
+package com.goofy.tunabank.v1.account;
+
+import com.goofy.tunabank.v1.domain.Enum.AccountType;
+
+public class InvalidAccountIdOrTypeException extends RuntimeException {
+
+    public InvalidAccountIdOrTypeException(Long accountId, AccountType accountType) {
+        super(String.format("유효하지 않은 계좌 정보 id : %d , type : %s", accountId, accountType));
+    }
+}
