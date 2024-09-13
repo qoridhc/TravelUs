6ee05b56399fbaa61b555c1a65241a5c1b603826
이진주 <dlwlswn3539@naver.com>
2024-09-13 15:40:47 +0900

    [BANK] Feat: 가맹점 도메인 구현 및 초기 데이터 삽입 구현

--- bank/src/main/java/com/goofy/tunabank/v1/domain/Enum/MerchantCategory.java
+++ bank/src/main/java/com/goofy/tunabank/v1/domain/Enum/MerchantCategory.java
@@ -3,5 +3,5 @@ package com.goofy.tunabank.v1.domain.Enum;
 public enum MerchantCategory {
 
   //FOOD:음식점, ACCOMODATION:숙박, TRANSPORTATION:교통, SHOPPING:쇼핑, OTHER: 기타
-  FOOD,ACCOMODATION,TRANSPORTATION,SHOPPING,OTHER
+  FOOD, ACCOMODATION, TRANSPORTATION, SHOPPING, OTHER
 }
