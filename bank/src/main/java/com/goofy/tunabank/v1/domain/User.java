b46bd020f17086ce732c560e63929a0242bcc9f3
허동원 <heo_dongwon@naver.com>
2024-09-13 16:00:21 +0900

    [BE] Feat: 스프링 시큐리티 + 유저 추가  - 필터 클래스 추가  - 유저 도메인 추가  - 유저 타입(Role) Enum 구현  - 시큐리티 설정 추가  - 종속성 주석 제거

---
+++ bank/src/main/java/com/goofy/tunabank/v1/domain/User.java
@@ -0,0 +1,40 @@
+package com.goofy.tunabank.v1.domain;
+
+import com.goofy.tunabank.v1.domain.Enum.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, name = "email")
+  private String email;
+
+  @Column(name = "is_exit")
+  private Boolean isExit;
+
+  @Column(name = "role")
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Column(name = "create_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "update_at")
+  private LocalDateTime updateAt;
+
+  @Column(name = "exit_at")
+  private LocalDateTime exitAt;
+}
