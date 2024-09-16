782b73578355c4afa66659a36d8bc2cef8742726
이진주 <dlwlswn3539@naver.com>
2024-09-16 12:18:23 +0900

    [BANK] Feat: 매퍼 구현

---
+++ bank/src/main/java/com/goofy/tunabank/v1/mapper/TransactionMapper.java
@@ -0,0 +1,20 @@
+package com.goofy.tunabank.v1.mapper;
+
+import com.goofy.tunabank.v1.domain.TransactionHistory;
+import com.goofy.tunabank.v1.dto.transaction.response.TransactionResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TransactionMapper {
+
+  @Mapping(target = "transactionHistoryId", source = "th.id")
+  @Mapping(target = "transactionType", source = "th.transactionType")
+  @Mapping(target = "accountNo", source = "th.account.accountNo")
+  @Mapping(target = "transactionAt", source = "th.transactionAt")
+  @Mapping(target = "amount", source = "th.amount")
+  @Mapping(target = "balance", source = "th.balance")
+  @Mapping(target = "summary", source = "th.summary")
+  TransactionResponseDto convertTransactionHistoryToTransactionResponseDto(
+      TransactionHistory th);
+}
