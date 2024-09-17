93b17fbdc6b979edaa1118be2bffe4840876433c
허동원 <heo_dongwon@naver.com>
2024-09-18 02:57:20 +0900

    [BANK] Fix: 스프링 시큐리티 버그 수정 request body stream 캐싱 설정 (filter에서 body를 stream으로 읽으면 뒤에 dispather servlet 등에서 못 읽어서 캐싱 처리)

--- bank/src/main/java/com/goofy/tunabank/v1/filter/KeyAutheticationFilter.java
+++ bank/src/main/java/com/goofy/tunabank/v1/filter/KeyAutheticationFilter.java
@@ -2,16 +2,20 @@ package com.goofy.tunabank.v1.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goofy.tunabank.v1.domain.Key;
-import com.goofy.tunabank.v1.dto.ApiRequestBody;
 import com.goofy.tunabank.v1.exception.auth.ApiKeyNotFoundException;
+import com.goofy.tunabank.v1.exception.auth.AuthException;
 import com.goofy.tunabank.v1.provider.KeyProvider;
 import com.goofy.tunabank.v1.repository.KeyRepository;
+import com.goofy.tunabank.v1.util.CachedBodyHttpServletRequest;
+import com.goofy.tunabank.v1.util.LogUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -26,37 +30,57 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 public class KeyAutheticationFilter extends OncePerRequestFilter {
 
+  private final static String REQUEST_HEADER = "header";
+  private final static String REQUEST_APIKEY = "apiKey";
+
   private final ObjectMapper objectMapper = new ObjectMapper();
   private final KeyRepository keyRepository;
   private final KeyProvider keyProvider;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws ServletException, IOException
-  {
-    // 헤더 파싱하기
-    ApiRequestBody.Header header = resolveKey(request);
+      throws IOException, ServletException {
 
-    // 키 존재 유무 검사
-    Key key = keyRepository.findByKeyValue(header.getApikey()).orElseThrow(
-        () -> new ApiKeyNotFoundException(header.getApikey())
-    );
+    // CachedBodyHttpServletRequest로 요청 본문 캐싱
+    CachedBodyHttpServletRequest cachedRequest = new CachedBodyHttpServletRequest(request);
 
-    // api key 유효성 검사 & 인증 정보 설정
-    if(keyProvider.validateKey(key)) {
-      Authentication auth = getAuthentication(key.getUser());
-      SecurityContextHolder.getContext().setAuthentication(auth);
+    try {
+      // api key 파싱
+      String apiKey = resolveHeader(cachedRequest).orElseThrow(
+          () -> new ApiKeyNotFoundException("Api Key is missing", "NULL")
+      );
+
+      // 키 존재 유무 검사
+      Key key = keyRepository.findByKeyValue(apiKey).orElseThrow(
+          () -> new ApiKeyNotFoundException(apiKey)
+      );
+
+      // api key 유효성 검사 & 인증 정보 설정
+      if (keyProvider.validateKey(key)) {
+        Authentication auth = getAuthentication(key.getUser());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+      }
+    } catch (AuthException e) {
+      LogUtil.warn(e.getInfo());
+      cachedRequest.setAttribute("exceptionMsg", e.getMessage());  // cachedRequest에 예외 설정
+    } catch (Exception e) {
+      e.printStackTrace();
+      cachedRequest.setAttribute("exceptionMsg", e.getMessage());  // cachedRequest에 예외 설정
+    } finally {
+      filterChain.doFilter(cachedRequest, response);
     }
-
-    // 다음 필터로 넘어가기
-    filterChain.doFilter(request, response);
   }
 
-
   // 요청(헤더) 파싱하기
-  private ApiRequestBody.Header resolveKey(HttpServletRequest request) throws IOException {
-    ApiRequestBody apiRequestBody = objectMapper.readValue(request.getInputStream(), ApiRequestBody.class);
-    return apiRequestBody.getHeader();
+  private Optional<String> resolveHeader(HttpServletRequest request) throws IOException {
+    Map<String, Object> bodyMap = objectMapper.readValue(request.getInputStream(), Map.class);
+
+    // 헤더를 Optional로 처리하여 값이 없을 경우 빈 Optional 반환
+    return Optional.ofNullable(
+            (Map<String, String>) bodyMap.get(REQUEST_HEADER))
+        .map(headerMap -> headerMap.get(REQUEST_APIKEY))
+        .filter(apiKey -> apiKey != null && !apiKey.isEmpty()
+        );
   }
 
   // 인증 정보 생성하기
