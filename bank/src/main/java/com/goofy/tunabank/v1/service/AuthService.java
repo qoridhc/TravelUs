8188fd6e525c0da099425004f9ecda376d298543
허동원 <heo_dongwon@naver.com>
2024-09-18 22:25:52 +0900

    [BANK] Feat: API KEY 기발급 개수 검증 (API KEY 발급)

--- bank/src/main/java/com/goofy/tunabank/v1/service/AuthService.java
+++ bank/src/main/java/com/goofy/tunabank/v1/service/AuthService.java
@@ -5,11 +5,14 @@ import com.goofy.tunabank.v1.domain.Key;
 import com.goofy.tunabank.v1.domain.User;
 import com.goofy.tunabank.v1.dto.auth.ApiKeyIssueReponseDto;
 import com.goofy.tunabank.v1.dto.auth.ApiKeyIssueRequestDto;
+import com.goofy.tunabank.v1.exception.auth.ApiKeyLimitExceededException;
 import com.goofy.tunabank.v1.exception.auth.UserNotFoundException;
 import com.goofy.tunabank.v1.provider.KeyProvider;
 import com.goofy.tunabank.v1.repository.KeyRepository;
 import com.goofy.tunabank.v1.repository.UserRepository;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,7 +33,11 @@ public class AuthService {
         () -> new UserNotFoundException(request)
     );
 
-
+    // 2. 발급한 API KEY 개수가 5개 이상인 경우 발급 X
+    int len = filterApiKeyType(user.getKeys()).size();
+    if(len >= 5){
+      throw new ApiKeyLimitExceededException(request.getManagerId());
+    }
 
     // 2. API KEY 발급
     String keyValue = keyProvider.generateApiKey();
@@ -46,5 +53,11 @@ public class AuthService {
         .createAt(LocalDateTime.now())
         .expireAt(LocalDateTime.now().plusYears(1))
         .build();
+  }
+
+  public List<Key> filterApiKeyType(List<Key> keys) {
+    return keys.stream()
+        .filter(key -> key.getType() == KeyType.API)
+        .collect(Collectors.toList());
   }
 }
