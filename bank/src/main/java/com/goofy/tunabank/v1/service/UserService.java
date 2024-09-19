13f22f6b9e234611574358b90909898758826bd2
허동원 <heo_dongwon@naver.com>
2024-09-19 17:20:44 +0900

    [BE] Feat: 유저 조회 비즈니스 로직 구현 (은행 회원 조회)
    Key
     - 만료 키 갱신 메서드 구현
    
    User
     - 유효 유저 키 조회 메서드 구현
    
    UserService
     - 비즈니스 로직 구현
    
    UserMapper
     - 매퍼 메서드 구현

--- bank/src/main/java/com/goofy/tunabank/v1/service/UserService.java
+++ bank/src/main/java/com/goofy/tunabank/v1/service/UserService.java
@@ -6,10 +6,17 @@ import com.goofy.tunabank.v1.domain.Key;
 import com.goofy.tunabank.v1.domain.User;
 import com.goofy.tunabank.v1.dto.user.UserJoinRequestDto;
 import com.goofy.tunabank.v1.dto.user.UserJoinResponseDto;
+import com.goofy.tunabank.v1.dto.user.UserSearchRequestDto;
+import com.goofy.tunabank.v1.dto.user.UserSearchResponseDto;
+import com.goofy.tunabank.v1.exception.auth.UserNotFoundException;
+import com.goofy.tunabank.v1.exception.user.UserKeyExpiredException;
 import com.goofy.tunabank.v1.exception.user.EmailDuplicatedException;
+import com.goofy.tunabank.v1.mapper.UserMapper;
 import com.goofy.tunabank.v1.provider.KeyProvider;
 import com.goofy.tunabank.v1.repository.KeyRepository;
 import com.goofy.tunabank.v1.repository.UserRepository;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +29,8 @@ public class UserService {
   private final UserRepository userRepository;
   private final KeyRepository keyRepository;
   private final KeyProvider keyProvider;
+
+  private final UserMapper userMapper;
 
   public UserJoinResponseDto createUser(UserJoinRequestDto request) {
 
@@ -45,11 +54,28 @@ public class UserService {
     keyRepository.save(createdKey);
 
     // 5. 응답
-    return UserJoinResponseDto.builder()
-        .userId(createdUser.getEmail())
-        .userKey(createdKey.getValue())
-        .created(createdUser.getCreatedAt())
-        .modified(createdUser.getUpdateAt())
-        .build();
+    return userMapper.toUserJoinResponseDto(createdUser, createdKey);
+  }
+
+  public UserSearchResponseDto searchUser(UserSearchRequestDto request) {
+
+    // 유저 조회
+    User user = userRepository.findByEmail(request.getUserId()).orElseThrow(
+        () -> new UserNotFoundException(request.getUserId())
+    );
+
+    // 유저 키 조회
+    Key key = user.getValidUserKey().orElseThrow(
+        () -> new UserKeyNotFoundException(request.getUserId())
+    );
+
+    // 유저 키 만료시, 자동 재발급
+    if(key.getExpireAt().isBefore(LocalDateTime.now())) {
+      String userKey = keyProvider.generateUserKey();
+      key.updateKey(userKey);
+    }
+
+    // 반환
+    return userMapper.toUserSearchResponseDto(user, key);
   }
 }
