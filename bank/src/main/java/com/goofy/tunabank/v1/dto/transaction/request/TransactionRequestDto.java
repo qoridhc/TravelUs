2d51e829e0bbb774e2fad11ffe38d3ed3208d301
이진주 <dlwlswn3539@naver.com>
2024-09-19 23:20:52 +0900

    [BANK] Feat: 거래용 헤더 추가

--- bank/src/main/java/com/goofy/tunabank/v1/dto/transaction/request/TransactionRequestDto.java
+++ bank/src/main/java/com/goofy/tunabank/v1/dto/transaction/request/TransactionRequestDto.java
@@ -1,12 +1,14 @@
 package com.goofy.tunabank.v1.dto.transaction.request;
 
-import com.goofy.tunabank.v1.domain.Enum.AccountType;
 import com.goofy.tunabank.v1.domain.Enum.TransactionType;
 import lombok.Data;
 
 @Data
 public class TransactionRequestDto {
-
+  
+  //헤더
+  private TransactionHeader header;
+  
   //계좌 Id
   private Long accountId;
 
