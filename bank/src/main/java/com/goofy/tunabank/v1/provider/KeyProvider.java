f85c6b102a2e067d89b77762b3041a35aec44530
허동원 <heo_dongwon@naver.com>
2024-09-19 17:18:53 +0900

    [BE] Feat: 예외 처리 (은행 회원 조회)
    ApiKeyExpiredException
    - api key 만료 시 발생하는 예외(이름 변경)
    
    UserKeyNotFoundException
     - 회원 가입된 유저가 user key를 가지고 있지 않을시 발생하는 예외(추가 구현)
    
    UserNotFoundException
     - 가입 되지 않은 메일로 유저 조회 시 발생되는 예외
    
     UserKeyExpiredException
      - user key 만료시 발생하는 예외

--- bank/src/main/java/com/goofy/tunabank/v1/provider/KeyProvider.java
+++ bank/src/main/java/com/goofy/tunabank/v1/provider/KeyProvider.java
@@ -2,7 +2,7 @@ package com.goofy.tunabank.v1.provider;
 
 import com.goofy.tunabank.v1.domain.Enum.KeyStatus;
 import com.goofy.tunabank.v1.domain.Key;
-import com.goofy.tunabank.v1.exception.auth.ExpiredApiKeyException;
+import com.goofy.tunabank.v1.exception.auth.ApiKeyExpiredException;
 import com.goofy.tunabank.v1.exception.auth.InvalidApiKeyException;
 import com.goofy.tunabank.v1.repository.KeyRepository;
 import java.security.SecureRandom;
@@ -30,7 +30,7 @@ public class KeyProvider {
 
     // 만료 기간 검사
     if(key.getExpireAt().isBefore(LocalDateTime.now())) {
-      throw new ExpiredApiKeyException(key.getValue(), key.getExpireAt());
+      throw new ApiKeyExpiredException(key.getValue(), key.getExpireAt());
     }
     return true;
   }
