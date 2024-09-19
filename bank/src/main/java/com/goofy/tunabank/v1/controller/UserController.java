c105349e33da509fd7a00550d5dd324966f796c3
허동원 <heo_dongwon@naver.com>
2024-09-19 17:19:30 +0900

    [BE] Feat: 유저 조회 요청/ 응답 처리 (은행 회원 조회)

--- bank/src/main/java/com/goofy/tunabank/v1/controller/UserController.java
+++ bank/src/main/java/com/goofy/tunabank/v1/controller/UserController.java
@@ -2,9 +2,13 @@ package com.goofy.tunabank.v1.controller;
 
 import com.goofy.tunabank.v1.dto.user.UserJoinRequestDto;
 import com.goofy.tunabank.v1.dto.user.UserJoinResponseDto;
+import com.goofy.tunabank.v1.dto.user.UserSearchRequestDto;
+import com.goofy.tunabank.v1.dto.user.UserSearchResponseDto;
 import com.goofy.tunabank.v1.service.UserService;
 import com.goofy.tunabank.v1.util.LogUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +26,14 @@ public class UserController {
   public ResponseEntity joinNewUesr(@RequestBody UserJoinRequestDto request) {
     LogUtil.info("회원 가입 요청", request.getUserId());
     UserJoinResponseDto response = userService.createUser(request);
-    return ResponseEntity.ok(response);
+    return new ResponseEntity(response, HttpStatus.CREATED);
+  }
+
+  @PostMapping("/search")
+  public ResponseEntity searchUser(@RequestBody UserSearchRequestDto request){
+    LogUtil.info("유저 조회 요청", request.getUserId());
+    UserSearchResponseDto response = userService.searchUser(request);
+    return new ResponseEntity(response, HttpStatus.CREATED);
   }
 
 }
