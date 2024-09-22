7488658182971896688c77d679732f55e2efe043
허동원 <heo_dongwon@naver.com>
2024-09-22 16:11:39 +0900

    [BANK] Feat: 유저 <-> 통장 <-> 카드 연관관계 작성 (카드 결제)

--- bank/src/main/java/com/goofy/tunabank/v1/domain/Card.java
+++ bank/src/main/java/com/goofy/tunabank/v1/domain/Card.java
@@ -1,7 +1,6 @@
 package com.goofy.tunabank.v1.domain;
 
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -24,7 +22,7 @@ public class Card {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long cardId;
 
-  @OneToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "account_id")
   private Account account;
 
