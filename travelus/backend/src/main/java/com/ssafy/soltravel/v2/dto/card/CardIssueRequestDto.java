efee1665b4b6f84e027dc0910bcddf0218bdcda5
허동원 <heo_dongwon@naver.com>
2024-09-23 14:06:42 +0900

    [BE] Feat: 카드 발급 요청(카드 발급)
    CardIssueRequestDto
     - Header -> BankHeader 로 변경
    
    CardService
     - bank api 호출 및 응답

--- travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/card/CardIssueRequestDto.java
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/card/CardIssueRequestDto.java
@@ -1,9 +1,21 @@
 package com.ssafy.soltravel.v2.dto.card;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.soltravel.v2.common.BankHeader;
+import com.ssafy.soltravel.v2.common.Header;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Null;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardIssueRequestDto {
   @NotEmpty
   @Length(min = 8, max = 8)
@@ -12,4 +24,8 @@ public class CardIssueRequestDto {
   @NotEmpty
   @Length(min = 17, max = 17)
   private String withdrawalAccountNo;  // 출금 계좌 번호
+
+  @Null
+  @JsonProperty("Header")
+  private BankHeader header;
 }
--- travelus/backend/src/main/java/com/ssafy/soltravel/v2/service/card/CardService.java
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/service/card/CardService.java
@@ -1,9 +1,19 @@
 package com.ssafy.soltravel.v2.service.card;
 
 
+import com.ssafy.soltravel.v2.common.BankHeader;
+import com.ssafy.soltravel.v2.domain.User;
 import com.ssafy.soltravel.v2.dto.card.CardIssueRequestDto;
-import com.ssafy.soltravel.v2.dto.card.CardIssueResponseDto;
+import com.ssafy.soltravel.v2.dto.card.CardResponseDto;
+import com.ssafy.soltravel.v2.exception.UserNotFoundException;
+import com.ssafy.soltravel.v2.repository.UserRepository;
+import com.ssafy.soltravel.v2.util.LogUtil;
+import com.ssafy.soltravel.v2.util.SecurityUtil;
+import com.ssafy.soltravel.v2.util.WebClientUtil;
+import java.time.LocalDateTime;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +22,52 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CardService {
 
+  private static final String DEFAULT_REQUEST_URI = "/card";
 
-  public CardIssueResponseDto createNewCard(CardIssueRequestDto request) {
+  private final WebClientUtil webClientUtil;
+  private final Map<String, String> apiKeys;
+  private final UserRepository userRepository;
 
-    
-    return null;
+  public CardResponseDto createNewCard(CardIssueRequestDto request) {
+
+    // 유저 조회
+    Long userId = SecurityUtil.getCurrentUserId();
+    User user = userRepository.findByUserId(userId).orElseThrow(
+        () -> new UserNotFoundException(userId)
+    );
+
+    // 요청용 body 수정
+    request.setHeader(
+        BankHeader.createHeader(
+            apiKeys.get("API_KEY"),
+            user.getUserKey()
+        )
+    );
+
+    // 은행 요청
+    LogUtil.info("은행 요청", request);
+    String requestUri = String.format("%s/issue", DEFAULT_REQUEST_URI);
+    ResponseEntity<Map<String, Object>> response = webClientUtil.request(
+        requestUri, request, CardIssueRequestDto.class
+    );
+
+    // 응답
+    response.getBody().get("");
+    return toDto(response.getBody());
+  }
+
+  private CardResponseDto toDto(Map<String, Object> response) {
+    CardResponseDto dto = CardResponseDto.builder()
+        .cardNo(response.get("cardNo").toString())
+        .cvc(response.get("cvc").toString())
+        .cardUniqueNo(response.get("cardUniqueNo").toString())
+        .cardIssuerName(response.get("cardIssuerName").toString())
+        .cardName(response.get("cardName").toString())
+        .cardDescription(response.get("cardDescription").toString())
+        .cardExpiryDate(response.get("cardExpiryDate").toString())
+        .withdrawalAccountNo(response.get("withdrawalAccountNo").toString())
+        .build();
+
+    return dto;
   }
 }
