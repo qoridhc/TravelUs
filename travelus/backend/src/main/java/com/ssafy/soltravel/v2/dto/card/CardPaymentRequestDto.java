f89f902c7b164ae0cae78eeba9dc6f69d7b0b30a
허동원 <heo_dongwon@naver.com>
2024-09-25 10:27:29 +0900

    [BE] Docs: Swagger 작성
    Card
     - 카드 결제, 발급, 조회 Swagger 작성

--- travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/card/CardPaymentRequestDto.java
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/card/CardPaymentRequestDto.java
@@ -2,6 +2,8 @@ package com.ssafy.soltravel.v2.dto.card;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.soltravel.v2.common.BankHeader;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -20,25 +22,33 @@ public class CardPaymentRequestDto {
 
   @NotEmpty
   @Length(min = 16, max = 16)
+  @Schema(description = "카드 번호", example = "1234567812345678", required = true)
   private String cardNo;
 
   @NotEmpty
   @Length(min = 3, max = 3)
+  @Schema(description = "카드의 CVC 번호", example = "123", required = true)
   private String cvc;
 
   @NotNull
   @Positive
+  @Schema(description = "결제할 가맹점의 ID", example = "1", required = true)
   private Long merchantId;
 
-  @NotNull @Positive
+  @NotNull
+  @Positive
+  @Schema(description = "결제 금액", example = "20.00", required = true)
   private Double paymentBalance;
 
   @Null
+  @Hidden
   private String transactionId;
 
   @NotEmpty
+  @Schema(description = "통화 코드 (예: USD, KRW 등)", example = "USD", required = true)
   private String currencyCode;
 
+  @Schema(hidden = true)
   @Null
   @JsonProperty("Header")
   BankHeader header;
