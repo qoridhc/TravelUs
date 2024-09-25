3108650b399b901afcd33f0ebefb25793719bfb8
이성현 <vkse1934@naver.com>
2024-09-25 14:34:25 +0900

    [BE] Feat: 모임 참여 시 master 권한 false 기본 설정

--- travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/group/request/CreateParticipantRequestDto.java
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/group/request/CreateParticipantRequestDto.java
@@ -8,25 +8,20 @@ import lombok.Data;
 @Builder
 public class CreateParticipantRequestDto {
 
-    @Schema(description = "참여자의 유저 ID", example = "1")
-    private Long userId;
-
     @Schema(description = "모임 ID", example = "1")
     private Long groupId;
 
-    @Schema(description = "그룹장 여부 (true: 그룹장, false: 일반 참여자)", example = "true")
-    private boolean isMaster;
+//    @Schema(description = "그룹장 여부 (true: 그룹장, false: 일반 참여자)", example = "false")
+//    private boolean isMaster;
 
     @Schema(description = "참여자의 개인 계좌 번호", example = "002-6157654-209")
     private String personalAccountNo;
 
     // CreateGroupRequestDto로 CreateParticipantRequestDto 생성하는 팩토리 메서드
-    public static CreateParticipantRequestDto createDto(Long userId, Long groupId, boolean isMaster, String personalAccountNo) {
+    public static CreateParticipantRequestDto createDto(Long groupId, boolean isMaster, String personalAccountNo) {
 
         return CreateParticipantRequestDto.builder()
-            .userId(userId)
             .groupId(groupId)
-            .isMaster(isMaster)
             .personalAccountNo(personalAccountNo)
             .build();
     }
