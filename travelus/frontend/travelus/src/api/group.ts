c5dd74b16a76d471c38fd37711976e8870242ca3
이예림 <yerim5988jg@naver.com>
2024-09-25 17:11:41 +0900

    [FE] Style: 필요없는 콘솔 출력 삭제

--- travelus/frontend/travelus/src/api/group.ts
+++ travelus/frontend/travelus/src/api/group.ts
@@ -4,7 +4,6 @@ import { meetingAccountInfo } from "../types/meetingAccount";
 export const groupApi = {
   // 모임통장 개설
   createMeetingAccount: (data: meetingAccountInfo) => {
-    console.log(data)
     return api.post(`/groups/createGroup`, data);
   },
 };
