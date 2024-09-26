4b0a51d39b598c7a7c5f500826d5555e8c660e10
이예림 <yerim5988jg@naver.com>
2024-09-26 16:14:31 +0900

    [FE] Design: 모임 초대장 페이지 디자인

--- travelus/frontend/travelus/src/pages/meetingAccount/InvitationOfMeeting.tsx
+++ travelus/frontend/travelus/src/pages/meetingAccount/InvitationOfMeeting.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import Lottie from "lottie-react";
+import invitationAnimation from "../../lottie/invitationAnimation.json";
 
 const InvitationOfMeeting = () => {
-  return <div>초대장입니다.</div>;
+  const [groupLeader, setGroupLeader] = useState("이예림");
+
+  return (
+    <div className="h-full p-5 pb-8 flex flex-col justify-between">
+      <div>
+        <div className="flex items-center">
+          <IoIosArrowBack className="text-2xl" />
+        </div>
+
+        <div className="mt-32 flex flex-col items-center space-y-3">
+          <Lottie className="w-36" animationData={invitationAnimation} />
+          <div className="text-2xl text-center font-semibold">
+            <p>{groupLeader}님의 모임통장</p>
+            <p>초대장 도착!</p>
+          </div>
+        </div>
+      </div>
+
+      <button className="w-full h-14 text-lg rounded-xl tracking-wide text-white bg-[#1429A0]">
+        모임통장 참여하기
+      </button>
+    </div>
+  );
 };
 
 export default InvitationOfMeeting;
