bc631100a59ad259e717b7fe31f4e5b1d9152028
허동원 <heo_dongwon@naver.com>
2024-09-26 18:15:23 +0900

    [BE] Feat: 유효하지 않은 모임 코드 조회 에러 처리

---
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/ErrorResponseDto.java
@@ -0,0 +1,46 @@
+package com.ssafy.soltravel.v2.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class ErrorResponseDto {
+
+    @Schema(description = "응답 상태", example = "FAIL")
+    private String status;
+
+    @Schema(description = "응답 메시지", example = "요청 처리 완료.")
+    private String message;
+
+    @Schema(example = "601")
+    private String code;
+
+    public ErrorResponseDto() {
+        this.status = "SUCCESS";
+        this.message = "요청 처리 완료";
+    }
+
+    public ErrorResponseDto(String message, String code) {
+        this.status = "FAIL";
+        this.message = message;
+        this.code = code;
+    }
+
+    public static ResponseEntity<ErrorResponseDto> databaseError(String message) {
+        ErrorResponseDto responseBody = new ErrorResponseDto("Database Error", message);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+    public static ResponseEntity<ErrorResponseDto> validationFail(String message) {
+        ErrorResponseDto responseBody = new ErrorResponseDto("Validation Failed", message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
+}
