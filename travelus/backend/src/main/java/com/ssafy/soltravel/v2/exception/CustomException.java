123c53730115520d3b18ef3d6153b0936f7aab07
이성현 <vkse1934@naver.com>
2024-09-27 21:07:19 +0900

    [BE] Feat: 알림 전송 예외처리

--- travelus/backend/src/main/java/com/ssafy/soltravel/v2/exception/CustomException.java
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/exception/CustomException.java
@@ -4,37 +4,37 @@ import lombok.Data;
 
 @Data
 public abstract class CustomException extends RuntimeException {
-  private static final String DEFAULT_MESSAGE = "Internal Server Error";
-  private static final String DEFAULT_CODE = "INTERNAL_SERVER_ERROR";
-  private static final int DEFAULT_STATUS = 500;
 
-  private String info;
-  private String message;
-  private String code;
-  private int status;
+    private static final String DEFAULT_MESSAGE = "Internal Server Error";
+    private static final String DEFAULT_CODE = "INTERNAL_SERVER_ERROR";
+    private static final int DEFAULT_STATUS = 500;
 
-  public CustomException(String message, String code, int status, String info) {
-    super(message);
-    this.message = message;
-    this.code = code;
-    this.status = status;
-    this.info = info;
-  }
+    private String info;
+    private String message;
+    private String code;
+    private int status;
 
-  public CustomException() {
-    super(DEFAULT_MESSAGE);
-    this.message = DEFAULT_MESSAGE;
-    this.info = DEFAULT_MESSAGE;
-    this.code = DEFAULT_CODE;
-    this.status = DEFAULT_STATUS;
-  }
+    public CustomException(String message, String code, int status, String info) {
+        super(message);
+        this.message = message;
+        this.code = code;
+        this.status = status;
+        this.info = info;
+    }
 
-  public CustomException(String message, String code, int status) {
-    super(message);
-    this.message = message;
-    this.code = code;
-    this.status = status;
-    this.info = message;
-  }
+    public CustomException() {
+        super(DEFAULT_MESSAGE);
+        this.message = DEFAULT_MESSAGE;
+        this.info = DEFAULT_MESSAGE;
+        this.code = DEFAULT_CODE;
+        this.status = DEFAULT_STATUS;
+    }
+
+    public CustomException(String message, String code, int status) {
+        super(message);
+        this.message = message;
+        this.code = code;
+        this.status = status;
+    }
 
 }
