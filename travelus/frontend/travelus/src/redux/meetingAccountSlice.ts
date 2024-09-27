0a16651843e5962c25cced9539e6f1a4cbcddcee
이예림 <yerim5988jg@naver.com>
2024-09-27 10:53:39 +0900

    [FE] Feat: 모임통장, 트래블박스의 redux 변수, 함수 추가

--- travelus/frontend/travelus/src/redux/meetingAccountSlice.ts
+++ travelus/frontend/travelus/src/redux/meetingAccountSlice.ts
@@ -1,14 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { meetingAccountInfo, TravelboxInfo } from "../types/meetingAccount";
 
 export interface meetingAccountState {
   meetingName: string;
   meetingType: string;
+  meetingAccounInfo: meetingAccountInfo;
+  travelboxInfo: TravelboxInfo;
 }
 
 const initialState: meetingAccountState = {
   meetingName: "",
   meetingType: "",
+  meetingAccounInfo: {
+    groupAccountPassword: "",
+    groupName: "",
+    icon: "",
+  },
+  travelboxInfo: {
+    accountPassword: "",
+    accountNo: "",
+    currencyCode: "",
+  },
 }
 
 export const meetingAccountSilce = createSlice({
@@ -21,9 +34,15 @@ export const meetingAccountSilce = createSlice({
     setMeetingType: (state, action: PayloadAction<string>) => {
       state.meetingType = action.payload;
     },
+    setMeetingAccountInfo: (state, action: PayloadAction<meetingAccountInfo>) => {
+      state.meetingAccounInfo = action.payload;
+    },
+    setTravelboxInfo: (state, action: PayloadAction<TravelboxInfo>) => {
+      state.travelboxInfo = action.payload;
+    },
   },
 });
 
-export const { setMeetingName, setMeetingType } = meetingAccountSilce.actions;
+export const { setMeetingName, setMeetingType, setMeetingAccountInfo, setTravelboxInfo } = meetingAccountSilce.actions;
 
 export default meetingAccountSilce.reducer;
