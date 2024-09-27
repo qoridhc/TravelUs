f4e79c025663b567fc7f675c3a3e2102e8d8aa3f
이성현 <vkse1934@naver.com>
2024-09-27 14:01:30 +0900

    [BANK] Feat: Fcm 관련 DTO 생성

---
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/dto/notification/PostTokenReq.java
@@ -0,0 +1,21 @@
+package com.ssafy.soltravel.v2.dto.notification;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PostTokenReq {
+    
+    Long userId;
+
+    @NotBlank(message = "토큰을 입력해야 합니다.")
+    @Schema(description = "FCM Token", example = "")
+    String token;
+}
