4e05b708a3d8ce5db57b5f7f0e645484ed993913
이예림 <yerim5988jg@naver.com>
2024-09-27 10:52:39 +0900

    [FE] Feat: 트래블박스 생성 타입 선언

--- travelus/frontend/travelus/src/types/meetingAccount.ts
+++ travelus/frontend/travelus/src/types/meetingAccount.ts
@@ -2,4 +2,11 @@ export interface meetingAccountInfo {
   groupAccountPassword: string;
   groupName: string;
   icon: string;
+  groupId?: number;
+}
+
+export interface TravelboxInfo {
+  accountPassword: string;
+  accountNo: string;
+  currencyCode: string;
 }
