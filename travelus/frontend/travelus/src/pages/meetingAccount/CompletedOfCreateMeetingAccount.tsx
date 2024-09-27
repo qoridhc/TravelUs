5513c13060442f85ab1d16a5d41f3fa9f7efe0fe
이예림 <yerim5988jg@naver.com>
2024-09-27 10:55:12 +0900

    [FE] Feat: 모임통장 및 초대코드 생성 시, 필요한 데이를 redux store에서 가져오도록 수정

--- travelus/frontend/travelus/src/pages/meetingAccount/CompletedOfCreateMeetingAccount.tsx
+++ travelus/frontend/travelus/src/pages/meetingAccount/CompletedOfCreateMeetingAccount.tsx
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router";
 import { userApi } from "../../api/user";
 import { accountApi } from "../../api/account";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 const CompletedOfCreateMeetingAccount = () => {
   const navigate = useNavigate();
   const params = useParams();
-  const location = useLocation();
 
   const [userName, setUserName] = useState("");
   const [groupCode, setGroupCode] = useState("");
+  const meetingAccounInfo = useSelector((state: RootState) => state.meetingAccount.meetingAccounInfo);
 
   const getUserName = async () => {
     try {
       const response = await userApi.fetchUser();
-      console.log("이름 : ", response.data.name);
       setUserName(response.data.name);
     } catch (error) {
       console.log("user의 fetchUser : ", error);
@@ -23,8 +24,10 @@ const CompletedOfCreateMeetingAccount = () => {
 
   const getInvitationCode = async () => {
     try {
-      const response = await accountApi.fetchInvitationCode(location.state.groupId);
-      setGroupCode(response.data.groupCode);
+      if (meetingAccounInfo.groupId !== undefined) {
+        const response = await accountApi.fetchInvitationCode(meetingAccounInfo.groupId);
+        setGroupCode(response.data.groupCode);
+      }
     } catch (error) {
       console.log("account의 fetchInvitationCode : ", error);
     }
@@ -40,7 +43,7 @@ const CompletedOfCreateMeetingAccount = () => {
       templateId: 112239,
       templateArgs: {
         hostName: userName,
-        groupName: location.state.groupName,
+        groupName: meetingAccounInfo.groupName,
         code: groupCode,
       },
     });
