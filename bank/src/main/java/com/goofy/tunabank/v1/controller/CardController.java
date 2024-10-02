af4b3b1ae23df7aab8ff4e4bd692c1f2544522a6
이진주 <dlwlswn3539@naver.com>
2024-10-02 13:58:20 +0900

    [BANK] Feat: 카드번호조회 by accountNo 서비스용 API 구현

--- bank/src/main/java/com/goofy/tunabank/v1/controller/CardController.java
+++ bank/src/main/java/com/goofy/tunabank/v1/controller/CardController.java
@@ -4,10 +4,12 @@ import com.goofy.tunabank.v1.common.RecWrapper;
 import com.goofy.tunabank.v1.dto.card.CardIssueRequestDto;
 import com.goofy.tunabank.v1.dto.card.CardIssueResponseDto;
 import com.goofy.tunabank.v1.dto.card.CardListRequestDto;
-import com.goofy.tunabank.v1.dto.card.CardRequestDto;
-import com.goofy.tunabank.v1.dto.card.CardResponseDto;
+import com.goofy.tunabank.v1.dto.card.CardNoRequestDto;
+import com.goofy.tunabank.v1.dto.card.CardNoResponseDto;
 import com.goofy.tunabank.v1.dto.card.CardPaymentRequestDto;
 import com.goofy.tunabank.v1.dto.card.CardPaymentResponseDto;
+import com.goofy.tunabank.v1.dto.card.CardRequestDto;
+import com.goofy.tunabank.v1.dto.card.CardResponseDto;
 import com.goofy.tunabank.v1.service.CardService;
 import com.goofy.tunabank.v1.util.LogUtil;
 import java.util.List;
@@ -27,30 +29,37 @@ public class CardController {
   private final CardService cardService;
 
   @PostMapping("/issue")
-  public ResponseEntity issueNewCard(@RequestBody CardIssueRequestDto request){
+  public ResponseEntity issueNewCard(@RequestBody CardIssueRequestDto request) {
     LogUtil.info("카드 발급 요청", request);
     CardIssueResponseDto response = cardService.createNewCard(request);
     return new ResponseEntity(response, HttpStatus.CREATED);
   }
 
   @PostMapping("/list")
-  public ResponseEntity getCardList(@RequestBody CardListRequestDto request){
+  public ResponseEntity getCardList(@RequestBody CardListRequestDto request) {
     LogUtil.info("카드 조회 요청", request.getHeader());
     List<CardResponseDto> response = cardService.findAllCards(request);
     return new ResponseEntity(new RecWrapper<>(response), HttpStatus.OK);
   }
 
   @PostMapping("/payment")
-  public ResponseEntity makeCardPayment(@RequestBody CardPaymentRequestDto request){
+  public ResponseEntity makeCardPayment(@RequestBody CardPaymentRequestDto request) {
     LogUtil.info("카드 결제", request);
     CardPaymentResponseDto response = cardService.makeCardPayment(request);
     return new ResponseEntity(new RecWrapper<>(response), HttpStatus.CREATED);
   }
 
   @PostMapping("/search")
-  public ResponseEntity getCard(@RequestBody CardRequestDto request){
+  public ResponseEntity getCard(@RequestBody CardRequestDto request) {
     LogUtil.info("카드 조회 요청", request);
     CardResponseDto response = cardService.findCard(request);
+    return new ResponseEntity(new RecWrapper<>(response), HttpStatus.OK);
+  }
+
+  @PostMapping("/inquireCard")
+  public ResponseEntity inquireCard(@RequestBody CardNoRequestDto request) {
+    LogUtil.info("계좌에 연결된 카드 조회 요청");
+    CardNoResponseDto response = cardService.inquireCardNoByAccountNo(request);
     return new ResponseEntity(new RecWrapper<>(response), HttpStatus.OK);
   }
 }
---
+++ bank/src/main/java/com/goofy/tunabank/v1/dto/card/CardNoResponseDto.java
@@ -0,0 +1,11 @@
+package com.goofy.tunabank.v1.dto.card;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CardNoResponseDto {
+
+  private String cardNo;
+}
