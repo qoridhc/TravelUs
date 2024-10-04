5e4961afd9b3c4b140b8d22812b44d6babeb6fea
이예림 <yerim5988jg@naver.com>
2024-10-04 15:30:43 +0900

    [FE] Feat: 개별 정산 요청 api 추가

--- travelus/frontend/travelus/src/api/settle.ts
+++ travelus/frontend/travelus/src/api/settle.ts
@@ -1,9 +1,14 @@
 import api from "../lib/axios";
-import { SettlementRequest } from "../types/settlement";
+import { SettlementParticipant, SettlementRequest } from "../types/settlement";
 
 export const settlementApi = {
   // 정산 수행
   fetchSettlement: (data: SettlementRequest) => {
     return api.post(`/settlement`, data);
-  }
+  },
+
+  // 개별 정산 요청
+  fetchSettlementPersonal: (participants: SettlementParticipant[]) => {
+    return api.post(`/settlement/personal`, {participants});
+  },
 };
