06f04d8412329fc9d4e0ca481f5267970d175369
이성현 <vkse1934@naver.com>
2024-10-04 21:50:46 +0900

    [BE] Feat: 알림 -> 그룹 다대일 단방향 연관관계 설정

--- travelus/backend/src/main/java/com/ssafy/soltravel/v2/domain/TravelGroup.java
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/domain/TravelGroup.java
@@ -27,39 +27,39 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class TravelGroup {
 
-	@Id
-	@Column(name = "group_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long groupId;
+    @Id
+    @Column(name = "group_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
 
-	private String groupAccountNo;
+    private String groupAccountNo;
 
-	private String travelStartDate;
+    private String travelStartDate;
 
-	private String travelEndDate;
+    private String travelEndDate;
 
-	private String groupName;
+    private String groupName;
 
-	private String icon;
+    private String icon;
 
-	@CreatedDate
-	private LocalDateTime createdAt;
+    @CreatedDate
+    private LocalDateTime createdAt;
 
-	@LastModifiedDate
-	private LocalDateTime updatedAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
-	@OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Participant> participants;
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Participant> participants;
 
-	public static TravelGroup createGroupEntity(String accountNo, CreateGroupRequestDto requestDto){
-			TravelGroup travelGroup = TravelGroup.builder()
-				.groupAccountNo(accountNo)
-				.travelStartDate(requestDto.getTravelStartDate())
-				.travelEndDate(requestDto.getTravelEndDate())
-				.groupName(requestDto.getGroupName())
-				.icon(requestDto.getIcon())
-				.build();
+    public static TravelGroup createGroupEntity(String accountNo, CreateGroupRequestDto requestDto) {
+        TravelGroup travelGroup = TravelGroup.builder()
+            .groupAccountNo(accountNo)
+            .travelStartDate(requestDto.getTravelStartDate())
+            .travelEndDate(requestDto.getTravelEndDate())
+            .groupName(requestDto.getGroupName())
+            .icon(requestDto.getIcon())
+            .build();
 
-			return travelGroup;
-	}
+        return travelGroup;
+    }
 }
