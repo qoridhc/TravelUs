0d340b0cd83d3c597ab03fc81dad00aeafd18be1
이예림 <yerim5988jg@naver.com>
2024-10-07 21:25:02 +0900

    [FE] Design: 모임통장 개설 안내 페이지 디자인 수정

--- travelus/frontend/travelus/src/pages/meetingAccount/createMeetingAccount/PrepareOfCreateMeetingAccount.tsx
+++ travelus/frontend/travelus/src/pages/meetingAccount/createMeetingAccount/PrepareOfCreateMeetingAccount.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RiHome5Line } from "react-icons/ri";
+import { GoHome } from "react-icons/go";
 import { FaMoneyCheck } from "react-icons/fa";
 import { FaMoneyCheckDollar } from "react-icons/fa6";
 import { AiTwotoneExclamationCircle } from "react-icons/ai";
@@ -16,11 +16,11 @@ const PrepareOfCreateMeetingAccount = () => {
   ];
 
   return (
-    <div className="h-full flex flex-col justify-between">
-      <div className="flex flex-col space-y-5">
-        <div className="p-5 grid grid-cols-[1fr_8fr_1fr]">
+    <div className="h-full p-5 pb-8 flex flex-col justify-between">
+      <div className="grid gap-14">
+        <div className="grid grid-cols-[1fr_8fr_1fr]">
           <div className="flex items-center">
-            <RiHome5Line
+            <GoHome
               onClick={() => {
                 navigate("/");
               }}
@@ -30,7 +30,7 @@ const PrepareOfCreateMeetingAccount = () => {
           <p className="text-xl text-center font-semibold">개설 안내</p>
         </div>
 
-        <div className="p-5 grid gap-8">
+        <div className="grid gap-8">
           <div className="text-2xl font-semibold">
             <p>해외여행 올인원 모임통장</p>
             <p>개설에 대해 알려드립니다</p>
@@ -40,7 +40,7 @@ const PrepareOfCreateMeetingAccount = () => {
             <div className="flex space-x-5">
               <FaMoneyCheck className="mx-3 text-3xl text-[#565656]" />
               <div className="flex space-x-2">
-                <p className="text-[#0471E9] font-semibold">01</p>
+                <p className="text-[#1429A0] font-semibold">01</p>
                 <div>
                   <p className="font-semibold">모임통장 개설</p>
                   <p className="text-sm text-[#565656]">원화(KRW)로 관리되는 모임통장</p>
@@ -51,7 +51,7 @@ const PrepareOfCreateMeetingAccount = () => {
             <div className="flex space-x-5">
               <FaMoneyCheckDollar className="mx-3 text-3xl text-[#565656]" />
               <div className="flex space-x-2">
-                <p className="text-[#0471E9] font-semibold">02</p>
+                <p className="text-[#1429A0] font-semibold">02</p>
                 <div>
                   <p className="font-semibold">트래블박스 개설</p>
                   <p className="text-sm text-[#565656]">원화는 통화로 관리되는 박스</p>
@@ -76,13 +76,11 @@ const PrepareOfCreateMeetingAccount = () => {
         </div>
       </div>
 
-      <div className="px-5 py-10">
-        <button
-          className={`w-full py-3 text-white bg-[#0471E9] rounded-lg`}
-          onClick={() => navigate("/meeting/create/userinfo")}>
-          가입하기
-        </button>
-      </div>
+      <button
+        className={`w-full h-14 text-lg rounded-xl tracking-wide text-white bg-[#1429A0]`}
+        onClick={() => navigate("/meeting/create/userinfo")}>
+        가입하기
+      </button>
     </div>
   );
 };
