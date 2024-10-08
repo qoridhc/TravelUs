d6883f2dec6984ea5dba4b2e353325fde313785c
이성현 <vkse1934@naver.com>
2024-10-08 20:33:36 +0900

    [BE] Feat: 머니박스 삭제 요청 / 응답 DTO 생성

--- bank/src/main/java/com/goofy/tunabank/v1/dto/transaction/request/TransferMBRequestDto.java
+++ bank/src/main/java/com/goofy/tunabank/v1/dto/transaction/request/TransferMBRequestDto.java
@@ -2,32 +2,51 @@ package com.goofy.tunabank.v1.dto.transaction.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.goofy.tunabank.v1.common.Header;
+import com.goofy.tunabank.v1.domain.Account;
 import com.goofy.tunabank.v1.domain.Enum.CurrencyType;
 import com.goofy.tunabank.v1.domain.Enum.TransferType;
+import com.goofy.tunabank.v1.domain.MoneyBox;
+import com.goofy.tunabank.v1.dto.moneyBox.request.DeleteMoneyBoxRequestDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TransferMBRequestDto {
 
-  //헤더
-  @JsonProperty("Header")
-  private Header header;
+    //헤더
+    @JsonProperty("Header")
+    private Header header;
 
-  //이체 종류
-  private TransferType transferType;
+    //이체 종류
+    private TransferType transferType;
 
-  //계좌번호
-  private String accountNo;
+    //계좌번호
+    private String accountNo;
 
-  //계좌 비밀번호
-  private String accountPassword;
+    //계좌 비밀번호
+    private String accountPassword;
 
-  //변경 전 통화
-  private CurrencyType sourceCurrencyCode;
+    //변경 전 통화
+    private CurrencyType sourceCurrencyCode;
 
-  //변경 후 통화
-  private CurrencyType targetCurrencyCode;
+    //변경 후 통화
+    private CurrencyType targetCurrencyCode;
 
-  //신청 금액
-  private double transactionBalance;
+    //신청 금액
+    private double transactionBalance;
+
+    // 팩토리 메서드 정의
+    public static TransferMBRequestDto from(Account account, MoneyBox moneyBox, DeleteMoneyBoxRequestDto requestDto) {
+
+        return new TransferMBRequestDto(
+            requestDto.getHeader(),
+            TransferType.M,
+            account.getAccountNo(),
+            account.getAccountPassword(),
+            requestDto.getCurrencyCode(),
+            CurrencyType.KRW,
+            moneyBox.getBalance()
+        );
+    }
 }
