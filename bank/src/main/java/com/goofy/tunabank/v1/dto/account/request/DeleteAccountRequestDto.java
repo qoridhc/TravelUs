20b0865f730fed78c5d5d8c7bf5ebf3ed904030a
이성현 <vkse1934@naver.com>
2024-10-09 01:00:30 +0900

    [BE] Feat: 계좌 삭제 관련 DTO 생성

--- bank/src/main/java/com/goofy/tunabank/v1/dto/account/request/DeleteAccountRequestDto.java
+++ bank/src/main/java/com/goofy/tunabank/v1/dto/account/request/DeleteAccountRequestDto.java
@@ -2,9 +2,11 @@ package com.goofy.tunabank.v1.dto.account.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.goofy.tunabank.v1.common.Header;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DeleteAccountRequestDto {
 
     @JsonProperty("Header")
@@ -13,5 +15,7 @@ public class DeleteAccountRequestDto {
     private String accountNo;
 
     private String accountPassword;
+
+    private String refundAccountNo;
 
 }
