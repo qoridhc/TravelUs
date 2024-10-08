0a01d0638cf58e6d5b093fd1c3f25fe20cd574e8
이진주 <dlwlswn3539@naver.com>
2024-10-09 00:36:50 +0900

    [BE] Feat: 자동환전신청구현

---
+++ travelus/backend/src/main/java/com/ssafy/soltravel/v2/repository/TargetRateRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.soltravel.v2.repository;
+
+import com.ssafy.soltravel.v2.domain.TargetRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TargetRateRepository extends JpaRepository<TargetRate, Long> {
+
+}
