3f1b182ef6a35a5fe216f3a86cd71fe4140f0ffa
이성현 <vkse1934@naver.com>
2024-10-08 20:33:01 +0900

    [BE] Feat: 계좌 & 머니박스 조회 시 status ACTIVE 검사

--- bank/src/main/java/com/goofy/tunabank/v1/repository/MoneyBoxRepository.java
+++ bank/src/main/java/com/goofy/tunabank/v1/repository/MoneyBoxRepository.java
@@ -7,20 +7,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface MoneyBoxRepository extends JpaRepository<MoneyBox,Long> {
+public interface MoneyBoxRepository extends JpaRepository<MoneyBox, Long> {
 
-  @Query("SELECT mb FROM MoneyBox mb " +
-      "JOIN FETCH mb.currency c " +
-      "JOIN FETCH mb.account a " +
-      "WHERE a.id = :accountId " +
-      "AND c.currencyCode = :currencyCode")
-  Optional<MoneyBox> findMoneyBoxByAccountAndCurrency(@Param("accountId") Long accountId, @Param("currencyCode") CurrencyType currencyCode);
+    @Override
+    void deleteById(Long id);
 
-  @Query("SELECT mb FROM MoneyBox mb " +
-      "JOIN FETCH mb.currency c " +
-      "JOIN FETCH mb.account a " +
-      "WHERE a.accountNo = :accountNo " +
-      "AND c.currencyCode = :currencyCode")
-  Optional<MoneyBox> findMoneyBoxByAccountNoAndCurrency(@Param("accountNo") String accountNo, @Param("currencyCode") CurrencyType currencyCode);
+    @Query("SELECT mb FROM MoneyBox mb " +
+        "JOIN FETCH mb.currency c " +
+        "JOIN FETCH mb.account a " +
+        "WHERE a.id = :accountId " +
+        "AND c.currencyCode = :currencyCode " +
+        "AND mb.status = 'ACTIVE' " +
+        "AND a.status = 'ACTIVE'")
+    Optional<MoneyBox> findMoneyBoxByAccountAndCurrency(@Param("accountId") Long accountId,
+        @Param("currencyCode") CurrencyType currencyCode);
+
+    @Query("SELECT mb FROM MoneyBox mb " +
+        "JOIN FETCH mb.currency c " +
+        "JOIN FETCH mb.account a " +
+        "WHERE a.accountNo = :accountNo " +
+        "AND c.currencyCode = :currencyCode " +
+        "AND mb.status = 'ACTIVE' " +
+        "AND a.status = 'ACTIVE'")
+    Optional<MoneyBox> findMoneyBoxByAccountNoAndCurrency(@Param("accountNo") String accountNo,
+        @Param("currencyCode") CurrencyType currencyCode);
 
 }
